ec3252ae796a3d984535d901096adb768a37e916
mallardflo <mallard.flo@gmail.com>
2015-05-29 15:25:04 +0300

    OK

--- DSE/web/functions.js
+++ DSE/web/functions.js
@@ -19,6 +19,12 @@ var myOptions = {
   };
   map      = new google.maps.Map(document.getElementById('map'), myOptions);
   panel    = document.getElementById('panel');
+  
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
       
       var marker = new google.maps.Marker({
     position : pos,
@@ -60,7 +66,38 @@ var myOptions = {
   map      = new google.maps.Map(document.getElementById('map'), myOptions);
   panel    = document.getElementById('panel');
   
+  var marker = new google.maps.Marker({
+    position : latLng,
+    map      : map,
+    title    : "Lille"
+    //icon     : "marker_lille.gif" // Chemin de l'image du marqueur pour surcharger celui par défaut
+  });
   
+  var contentMarker = [
+      '<div id="containerTabs">',
+      '<div id="tabs">',
+      '<ul>',
+        '<li><a href="#tab-1"><span>Lorem</span></a></li>',
+        '<li><a href="#tab-2"><span>Ipsum</span></a></li>',
+        '<li><a href="#tab-3"><span>Dolor</span></a></li>',
+      '</ul>',
+      '<div id="tab-1">',
+        '<h3>Lille</h3><p>Suspendisse quis magna dapibus orci porta varius sed sit amet purus. Ut eu justo dictum elit malesuada facilisis. Proin ipsum ligula, feugiat sed faucibus a, <a href="http://www.google.fr">google</a> sit amet mauris. In sit amet nisi mauris. Aliquam vestibulum quam et ligula pretium suscipit ullamcorper metus accumsan.</p>',
+      '</div>',
+      '<div id="tab-2">',
+       '<h3>Aliquam vestibulum</h3><p>Aliquam vestibulum quam et ligula pretium suscipit ullamcorper metus accumsan.</p>',
+      '</div>',
+      '<div id="tab-3">',
+        '<h3>Pretium suscipit</h3><ul><li>Lorem</li><li>Ipsum</li><li>Dolor</li><li>Amectus</li></ul>',
+      '</div>',
+      '</div>',
+      '</div>'
+  ].join('');
+
+  var infoWindow = new google.maps.InfoWindow({
+    content  : contentMarker,
+    position : latLng
+  });
   
   google.maps.event.addListener(marker, 'click', function() {
     infoWindow.open(map,marker);
